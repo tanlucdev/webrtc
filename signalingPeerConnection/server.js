eae3c24d06de6d7c20d3805eed15107596a3f2de
tanlucdev <tanluc.dev@gmail.com>
2024-05-20 22:08:14 +0700

    Setup https

--- signalingPeerConnection/server.js
+++ signalingPeerConnection/server.js
@@ -1,4 +1,21 @@
+const fs = require('fs')
+const https = require('https')
 const express = require('express')
 const app = express()
+const socketio = require('socket.io')
 app.use(express.static(__dirname))
-app.listen(8181)
+
+// generate with mkcert
+const key = fs.readFileSync('cert.key')
+const cert = fs.readFileSync('cert.crt')
+
+
+// change express setup to use https
+// pass the key and cert to createServer on https
+const expressServer = https.createServer({ cert, key }, app)
+// create socket.io server ... listen to express port
+const io = socketio(expressServer)
+expressServer.listen(8181)
+io.on('connection', (socket) => {
+  console.log("Someone has connected.")
+})
