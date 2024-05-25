b0a68b86f5f56352ecb73e1ee843cc3812c25796
tanlucdev <tanluc.dev@gmail.com>
2024-05-25 20:55:13 +0700

    Creating a JWT & link to simulate scheduling

--- teleLegalSite/teleLegal-back-end/expressRoutes.js
+++ teleLegalSite/teleLegal-back-end/expressRoutes.js
@@ -1,7 +1,29 @@
 // Nơi tất cả express stuff xảy ra
-
 const app = require('./server').app
+const jwt = require('jsonwebtoken')
+const linkSecret = 'sdjkdsjksd4ds2eqessad'
 
-app.get('/test', (req, res) => {
-  res.json("This is a test route")
+// Route này là của chúng ta
+// Gửi nó đi. Chúng ta sẽ print và past nó vào.
+// Nó sẽ thả chúng ta xuống trang React với thông tin đúng cho CLIENT1 để tạo yêu cầu
+app.get('/user-link', (req, res) => {
+
+  // Dữ liệu cho end-user's appt
+  const apptData = {
+    professionalFullName: "Kento De, X.F",
+    appDate: Date.now()
+  }
+
+  // chúng ta cần mã hóa dữ liệu trong token
+  // vì thế nó có thể được thêm vào url
+  const token = jwt.sign(apptData, linkSecret)
+  res.send('https://localhost:3000/join-video?token=' + token)
+
+  // res.json("This is a test route")
+})
+
+app.get('/validate-link', (req, res) => {
+  const token = req.query.token
+  const decodedData = jwt.verify(token, linkSecret)
+  res.json(decodedData)
 })
